d18d87d0ebebc134e920b30e434b50fda7e9bcb6
Ivan Soregashi <8psycho@gmail.com>
2024-08-18 18:57:00 +0500

    basic shell script

---
+++ Accountant.py
@@ -0,0 +1,23 @@
+from click_shell import shell
+import click, logging.config
+
+__author__ = "Ivan Shiriaev"
+__maintainer__ = "Ivan Shiriaev"
+__version__ = 0.1
+
+logging.config.fileConfig('config/log.conf')
+log = logging.getLogger("Accountant")
+
+
+@shell(prompt="Accountant > ", intro="Welcome!")
+def main():
+    log.info("main")
+
+
+@main.command("info")
+def get_account_info():
+    log.info("get_account_info")
+
+
+if __name__ == "__main__":
+    main()

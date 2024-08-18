5b1f70eba4fb8510662f4162e2f7641bdf4de3c9
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2024-08-18 21:27:46 +0500

    config optimization

--- Accountant.py
+++ Accountant.py
@@ -1,22 +1,38 @@
+import click
+import logging.config
+
 from click_shell import shell
-import click, logging.config
 
 __author__ = "Ivan Shiriaev"
 __maintainer__ = "Ivan Shiriaev"
-__version__ = 0.1
+__version__ = 0.11
 
 logging.config.fileConfig('config/log.conf')
 log = logging.getLogger("Accountant")
 
 
-@shell(prompt="Accountant > ", intro="Welcome!")
-def main():
-    log.info("main")
+@shell(prompt="Accountant > ", intro="Welcome!", chain=True)
+#@click.group(chain=True)
+@click.pass_context
+def main(ctx):
+    ctx.obj = {}
+    log.info(f"programs start")
 
 
-@main.command("info")
-def get_account_info():
-    log.info("get_account_info 2")
+@main.command("gqa")
+@click.pass_context
+def gqa(ctx): ctx.obj['environment'] = "goldenqa"
+
+
+@main.command("dev")
+@click.pass_context
+def dev(ctx): ctx.obj['environment'] = "goldendev"
+
+
+@main.command("show")
+@click.pass_context
+def show_context(ctx):
+    log.info(f"env {ctx.obj['environment']}")
 
 
 if __name__ == "__main__":

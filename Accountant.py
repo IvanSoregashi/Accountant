1b2ccfb51671bd420243a41e8e08142e5a2adfc2
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2024-08-18 22:05:26 +0500

    Integrated local storage

--- Accountant.py
+++ Accountant.py
@@ -2,11 +2,14 @@ import click
 import logging.config
 
 from click_shell import shell
+from LocalStorage import LocalStorage
+
+
+
 
 __author__ = "Ivan Shiriaev"
 __maintainer__ = "Ivan Shiriaev"
 __version__ = 0.11
-
 logging.config.fileConfig('config/log.conf')
 log = logging.getLogger("Accountant")
 
@@ -21,18 +24,25 @@ def main(ctx):
 
 @main.command("gqa")
 @click.pass_context
-def gqa(ctx): ctx.obj['environment'] = "goldenqa"
+def gqa(ctx):
+    ctx.obj['environment'] = "goldenqa"
+    ctx.obj['local'] = LocalStorage("goldenqa")
+    #load_dotenv(f"config/.env.goldenqa", override=True)
 
 
 @main.command("dev")
 @click.pass_context
-def dev(ctx): ctx.obj['environment'] = "goldendev"
+def dev(ctx):
+    ctx.obj['environment'] = "goldendev"
+    ctx.obj['local'] = LocalStorage("goldendev")
 
 
 @main.command("show")
 @click.pass_context
 def show_context(ctx):
     log.info(f"env {ctx.obj['environment']}")
+    log.info(f"first email: {next(iter(ctx.obj['local'].accounts.values()))['email']}")
+
 
 
 if __name__ == "__main__":

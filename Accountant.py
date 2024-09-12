a8d2b16575ce22f0ea55f9529a7de319d002eaae
Ivan Soregashi <IvanShiriaev2222@gmail.com>
2024-09-12 11:16:59 +0500

    Added helper messages, added registration options 3 and 4, will not share implementation for now

--- Accountant.py
+++ Accountant.py
@@ -2,12 +2,14 @@ import click
 import logging.config
 
 from click_shell import shell
+
+from Requests import register_ux3, directory_register
 from utils import *
 from Account import AccountGroup, Account
 
 __author__ = "Ivan Shiriaev"
 __maintainer__ = "Ivan Shiriaev"
-__version__ = 0.30
+__version__ = 0.31
 
 logging.config.fileConfig("config/log.conf")
 log = logging.getLogger("Accountant")
@@ -17,40 +19,48 @@ log = logging.getLogger("Accountant")
 #@click.group(chain=True)
 @click.pass_context
 def main(ctx):
+    """Main command group, that's it"""
     ctx.obj = {}
     log.info(f"We are starting here!")
-    ctx.call_on_close(lambda: print("exiting the app"))
+    ctx.call_on_close(lambda: log.info("exiting the app"))
 
 
 @main.command("qa")
 def qa():
+    """Setting up environment to work on qa"""
     ENV.QA.SET()
 
 
 @main.command("dev")
 def dev():
+    """Setting up environment to work on development"""
     ENV.DEV.SET()
 
 
 @main.command("prod")
 def prod():
+    """Setting up environment to work on production"""
     ENV.PROD.SET()
 
 
 @main.command("acc")
-#@click.option("--data", type=str, prompt="Enter email or userId", required=True)
+# @click.option("--data", type=str, prompt="Enter email or userId", required=True)
 @click.argument("data", type=str, required=True)
+# , help="Information to search account by, can be either email or userId")
 @click.pass_context
 def acc(ctx, data):
+    """Select account to work with.
+    First we will try to find it locally,
+    Then in DynamoDB if not found"""
     # Local Search
     acc = Account.get_local(data)
     if acc:
         log.debug(f"Account {data} was found locally")
         ctx.obj['account'] = acc
         return
-    # Confirming search in remote DB
-    click.echo("Should we check the database?")
-    if not confirm(): return
+    # Confirming search in remote DB, commented out for now, doing automatically
+    # click.echo("Should we check the database?")
+    # if not confirm(): return
     # Search in remote DB by the data type
     if is_usrId(data):
         acc = Account.from_userid(data)
@@ -61,24 +71,25 @@ def acc(ctx, data):
         return
     # account
     if acc:
-        print(acc)
         log.debug(f"Account {data} was found in remote db")
         ctx.obj['account'] = acc
-        click.echo("Save Account to disk?")
-        if confirm():
-            acc.save_local()
-            AccountGroup().save_accounts()
+        """click.echo("Save Account to disk?")
+        if confirm():"""
+        # lets save account automatically for now
+        acc.save_local()
+        AccountGroup().save_accounts()
     else:
         log.error(f"Account {data} was not found in remote db")
 
 
 @main.command("show")
 @click.option("-s", "--hash", is_flag=True, help="Print out hashed userId (for the LaunchDarkly).")
-@click.option("-f", "--full", is_flag=True, help="Print out Full account json.")
+@click.option("-f", "--full", is_flag=True, help="Print out raw account json.")
 @click.pass_context
 def show_context(ctx, hash, full):
+    """Show information about the account"""
     if 'account' not in ctx.obj:
-        log.error("not found")
+        log.error("Account was not selected yet")
         return
     click.echo(ctx.obj['account'])
     if full: click.echo(expand(dec_to_int(ctx.obj['account'].data)))
@@ -88,6 +99,7 @@ def show_context(ctx, hash, full):
 @main.command("reset")
 @click.pass_context
 def reset_context(ctx):
+    """Clear out the context, like selected account or list of accounts"""
     log.debug("resetting context")
     ctx.obj.clear()
 
@@ -95,6 +107,7 @@ def reset_context(ctx):
 @main.command("remove")
 @click.pass_context
 def remove_account(ctx):
+    """Remove the account from the list of saved accounts"""
     if 'account' in ctx.obj:
         ctx.obj['account'].remove()
     else:
@@ -104,6 +117,7 @@ def remove_account(ctx):
 @main.command("update")
 @click.pass_context
 def update_account(ctx):
+    """Refresh the account data from the database"""
     if 'account' in ctx.obj:
         ctx.obj['account'].refresh()
     else:
@@ -111,18 +125,30 @@ def update_account(ctx):
 
 
 @main.command("list")
-@click.argument("filter", nargs=-1)
+@click.argument("filter", nargs=-1)# , help="this flag is useless for now")
 @click.pass_context
 def list_accounts(ctx, filter):
+    """List the accounts,
+    narrow down the list by using 'filter' command"""
     if 'accounts' not in ctx.obj:
         ctx.obj['accounts'] = AccountGroup()
     click.echo(ctx.obj['accounts'].list_repr())
 
 
-@main.command("flt")
-@click.argument("args", required=True, nargs=-1, type=str)
+@main.command("filter")
+@click.argument("args", required=True, nargs=-1, type=str)# , help="list the filters")
 @click.pass_context
 def filter_list_argument(ctx, args):
+    f"""Filter the list of the accounts
+    view the list of your accounts with 'list' command
+    Filter by country code {tuple(COUNTRY_IP)}
+    Filter by geographical zone {tuple(REGION)}
+    Filter by environment dev, qa, etc
+    Filter by partner company
+    Filter by last update (not yet)
+    Filter by Devices (not yet)
+    Filter by provisioned devices (not yet)
+    """
     log.debug(str(args))
     if 'accounts' not in ctx.obj:
         ctx.obj['accounts'] = AccountGroup()
@@ -172,16 +198,57 @@ def get_engagements(ctx):
 @click.option("-t", "--type", required=True)
 @click.pass_context
 def create_account(ctx, email, country, language, type):
-    env = ctx.obj.get('environment', confirm_env())
+    if ENV.IS_NOT_SET():
+        ENV.CHOOSE().SET()
     match type:
-        case '3': Account.create_3(env, ensure_email(email), country, language)
-        case '4i': Account.create_4i(env, ensure_email(email), country, language)
-        case '4a': pass'''
+        case '3': userId = Requests.register_ux3(ensure_email(email), country.upper(), language.lower())
+        case '4o': Account.create_4i(ensure_email(email), country.upper(), language.lower())
+        case '4d': create_directory(ensure_email(email), country.upper(), language.lower())'''
 
 
-'''@main.command("unmigrate")
+@main.command("reg3")
+@click.option("-m", "--email", prompt=True, type=str, required=True, help="email address of the account")
+@click.option("-c", "--country", prompt=True, type=str, default="US", help="Country Code of the account")
+@click.option("-l", "--language", type=str, default="en", help="language assigned to the account")
+@click.pass_context
+def create_legacy_account(ctx, email, country, language):
+    """Register legacy account with custom country and language"""
+    userId = register_ux3(ensure_email(email), country.upper(), language.lower())
+    acc = Account.from_userid(userId)
+    ctx.obj['account'] = acc
+    acc.save_local()
+    AccountGroup().save_accounts()
+
+
+@main.command("reg4")
+@click.option("-m", "--email", prompt=True, type=str, required=True, help="email address of the account")
+@click.option("-c", "--country", prompt=True, type=str, default="US", help="Country Code of the account")
+@click.option("-l", "--language", type=str, default="en", help="language assigned to the account")
+@click.option("-v", "--verify", is_flag=True, help="auto-confirm the email address?")
+@click.option("-a", "--mfa", is_flag=True, help="Disable mfa? works only on dev environment right now")
+@click.option("-o", "--location", type=str, default="Home", help="Create default location")
+@click.pass_context
+def create_directory_account(ctx, email, country, language, verify, mfa, location):
+    """Register new account via directory API with custom country and language,
+    auto verify email, disable mfa (dev), create default location with the use of flags.
+    Corporate VPN is necessary for use of Directory API."""
+    userId = directory_register(
+        ensure_email(email),
+        country.upper(),
+        language.lower(),
+        verify,
+        mfa,
+        location)
+    acc = Account.from_userid(userId)
+    ctx.obj['account'] = acc
+    acc.save_local()
+    AccountGroup().save_accounts()
+
+
+@main.command("unmigrate")
 @click.pass_context
 def unmigrate(ctx):
+    """Unmigrate the currently selected account"""
     if "account" not in ctx.obj:
         log.error("Account was not specified")
         return
@@ -190,14 +257,16 @@ def unmigrate(ctx):
         log.error("Account had not been migrated")
         return
     resp = ctx.obj['account'].unmigrate()
-    (log.info if resp.ok else log.error)(f"Response: {str(resp.content)}")'''
+    (log.info if resp.ok else log.error)(f"Response: {str(resp.content)}")
 
 
 @main.command("exp")
 def experiment():
+    """Experimental function"""
     pass
 
 
 if __name__ == "__main__":
     main()
+
 
